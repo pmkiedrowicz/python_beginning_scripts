5e6ed133a22af3eb79e1b81736aa87f4bbdfcc17
pmkiedrowicz <pmkiedrowicz@gmail.com>
2019-09-18 10:04:38 +0200

    add random_choice

---
+++ random_choice.py
@@ -0,0 +1,9 @@
+'''
+Pick a random charcter from a given String
+'''
+import random
+
+st = "Emma is a good developer. Emma is also a writer, and output should be 2."
+
+out = random.choice(st)
+print(out)
